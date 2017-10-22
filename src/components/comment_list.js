8e40394ab39318256dbb50ee8e11f574974c7789
Luis Nesi <luisenesi@gmail.com>
2017-10-22 12:44:05 +0100

    Adding transition animation

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 const CommentList = (props)=>{	
-	const list=props.comments.map((comment,i)=><li key={i}>{comment}</li>);
+	const transitionOptions = {
+		//the name fade is the name of the first part of yje class name needed on the css
+		transitionName: "fade", 
+		transitionEnterTimeout:500,
+		transitionLeaveTimeout:500
+	};
+
+	const list=props.comments.map((comment,i)=>{
+		return (
+			<li className="list-group-item" key={i}>{comment}</li>
+			)
+	});
 	return(
-		<ul className="comment-list list-group">{list}</ul>
+		<ul className="comment-list list-group">
+			<ReactCSSTransitionGroup {...transitionOptions}>
+			{list}
+			</ReactCSSTransitionGroup>
+		</ul>
 		)
 }
 function mapStateToProps({comments}){
